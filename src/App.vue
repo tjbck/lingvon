aefa21893ba80d8b1834af8fa6f44c8863c81622
Timothy J. Beck <timothyjrbeck@gmail.com>
2019-08-18 00:41:30 +0900

    0.0.1c
    minor design change

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">Lingvon</b-navbar-brand>
+      <b-navbar-brand href="#">
+        <img src="logo.png" width="30" height="30" style="margin-right:5px"/>
+        <!-- Lingvon -->
+      </b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">About</b-nav-item>
+          <!-- <b-nav-item href="#">About</b-nav-item> -->
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
